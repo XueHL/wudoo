44b55fc0bdf820889a4502f9419f97e3f2470c97
rchbiu <rchbiu@localhost>
2009-05-29 16:55:07 +0400

    .

---
+++ Examples/Compile/CPP/UseExportHeaders/CM/sub-missions/build_useexphdr_asstatlib.py
@@ -0,0 +1,33 @@
+import pickle
+from wudoo.compile.cpp.Front import *
+
+project = Project(name = "UseExportHdr")
+
+MDL_FILE = moduleFile2basePath(__file__)
+project.setRoot(os.path.join("..", ".."), MDL_FILE)
+project.addSrcFolders(
+"""
+Src
+"""
+)
+
+### ### Dependings  ### ###
+addDependProjDir(os.path.join(MDL_FILE, "..", "..", "..", "ExportHeaders", "CM"))
+import build_exphdr
+exportHdrPrj = build_exphdr.getProject()
+project.addDependenceProject(exportHdrPrj, linkType = LinkTypes.STATIC_LIB)
+
+### ### ### ### ### ### ### ### ### ### 
+storrage = pickle.dumps(project)
+del project
+
+def getProject():
+	project = pickle.loads(storrage)
+	return project
+
+
+### ### ### ### ### ### ### ### ### ### 
+if (__name__ == "__main__"):
+	import os
+	project = getProject()
+	wdefaultBuild(project)
