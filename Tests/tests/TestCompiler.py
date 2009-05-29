4883f5b9d3e0266f447694e696e0b69988ed62b9
rchbiu <rchbiu@localhost>
2009-05-29 14:11:45 +0400

    .

--- Tests/tests/TestCompiler.py
+++ Tests/tests/TestCompiler.py
@@ -9,7 +9,13 @@ from tests.fakes.StoreCallsWillExecutor import StoreCallsWillExecutor
 class TestCompiler(unittest.TestCase):
 	def getSrc2ObjMap(self):
 		return self.__map
-	
+	def getProject(self):
+		return self
+	def getHdrFolders(self):
+		return []
+	def getExportHdrFolders(self):
+		return []
+
 	def testProjectRootPathSet(self):
 		we = StoreCallsWillExecutor()
 		compiler = GPPCompiler()
@@ -17,4 +23,4 @@ class TestCompiler(unittest.TestCase):
 		objItem = FSItem("C:/Work/Out/Obj/main.o");
 		self.__map = { srcItem: objItem }
 		compiler.compile(srcItem, self, we)
-		self.assertEquals(["g++ -c C:/Work/Src/main.cpp -o C:/Work/Out/Obj/main.o"], we.history)
+		self.assertEquals(["g++ -c \"C:/Work/Src/main.cpp\" -o \"C:/Work/Out/Obj/main.o\" "], we.history)
