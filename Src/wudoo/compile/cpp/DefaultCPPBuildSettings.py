0c64537e9891edd528c7b792c157e4e8eb281fd5
rchbiu <rchbiu@localhost>
2009-05-30 09:19:47 +0400

    .

--- Src/wudoo/compile/cpp/DefaultCPPBuildSettings.py
+++ Src/wudoo/compile/cpp/DefaultCPPBuildSettings.py
@@ -5,10 +5,15 @@ class DefaultCPPBuildSettings:
 	def __init__(self):
 		self.__objRoot = None
 		self.__exeFileFSItem = None
+		self.__dependenceBuildRoot = None
 
 	def setObjRoot(self, objRoot):
 		objRoot = os.path.abspath(objRoot)
 		self.__objRoot = objRoot
+
+	def setDependenceBuildRoot(self, dependenceBuildRoot):
+		dependenceBuildRoot = os.path.abspath(dependenceBuildRoot)
+		self.__dependenceBuildRoot = dependenceBuildRoot
 
 	def setExeFile(self, exeFile):
 		exeFile = os.path.abspath(exeFile)
@@ -19,3 +24,4 @@ class DefaultCPPBuildSettings:
 	def fillCompilation(self, compilation):
 		compilation.setObjRoot(self.__objRoot)
 		compilation.setBinDestFSItem(self.__exeFileFSItem)
+		compilation.setDependenceBuildRoot(self.__dependenceBuildRoot)
