6c10200f04a7305a612e564ec3acc15d42dea3e6
rchbiu <rchbiu@localhost>
2009-06-01 14:36:50 +0400

    .

--- Src/wudoo/compile/dependency/IDependency.py
+++ Src/wudoo/compile/dependency/IDependency.py
@@ -1,3 +1,12 @@
 class IDependency:
 	def getProject(self):
 		raise NotImplementedError()
+
+	def resolve(self, parentCompilation, willExecutor):
+		raise NotImplementedError()
+
+	def getBuildRoot(self):
+		raise NotImplementedError()
+
+	def getDependenceObjects(self):
+		raise NotImplementedError()
