90e40a9f324cad3eb51f35362ca416f03587d5c4
rchbiu <rchbiu@localhost>
2009-06-07 19:33:09 +0400

    .

--- Tests/tests/compile/examples/TestStoringCompilation.py
+++ Tests/tests/compile/examples/TestStoringCompilation.py
@@ -24,20 +24,21 @@ class TestStoringCompilation(unittest.TestCase):
 	
 	def testTheLibraryExample(self):
 		from wudoo.compile.cpp.Front import wsetupDefaultPathsFromRoot, wdefaultBuild
-		  
 		tmpDir = tempfile.mktemp()
 		tmp0 = os.path.join(tmpDir, "p0") 
-		tmp1 = os.path.join(tmpDir, "p1") 
+		tmp1 = os.path.join(tmpDir, "p1")
+		 
 		usr0prj = TestStoringCompilation.usr0prj.getProject()
 		def setupTmpdirCallback0(compilation):
 			wsetupDefaultPathsFromRoot(compilation, tmp0)
 		scwe = StoreCallsWillExecutor()
 		wdefaultBuild(usr0prj, setupTmpdirCallback0, scwe)
-		history = "@".join(scwe.history).replace(tmpDir, "__TMP__").split("@")
+		trunk = os.path.normpath(os.path.join(sys.path[0], "..")) 
+		history = "@".join(scwe.history).replace(trunk, "__TRUNK__").replace(tmpDir, "__TMP__").split("@")
 		self.assertEqual(
 			[
-			'g++ -c "D:\\Work\\_Projects\\Wudoo\\trunk-000\\Examples\\Compile\\CPP\\StoreBuildResults\\User0\\Src\\main.cpp" -o "__TMP__\\p0\\Obj\\Src\\main.o"  -I"D:\\Work\\_Projects\\Wudoo\\trunk-000\\Examples\\Compile\\CPP\\StoreBuildResults\\TheLibrary\\ExportHdr"', 
-			'g++ -c "D:\\Work\\_Projects\\Wudoo\\trunk-000\\Examples\\Compile\\CPP\\StoreBuildResults\\TheLibrary\\Src\\NameHolder.cpp" -o "__TMP__\\p0\\Outer\\TheLibrary\\Src\\NameHolder.o"  -I"D:\\Work\\_Projects\\Wudoo\\trunk-000\\Examples\\Compile\\CPP\\StoreBuildResults\\TheLibrary\\ExportHdr"', 
+			'g++ -c "__TRUNK__\\Examples\\Compile\\CPP\\StoreBuildResults\\User0\\Src\\main.cpp" -o "__TMP__\\p0\\Obj\\Src\\main.o"  -I"__TRUNK__\\Examples\\Compile\\CPP\\StoreBuildResults\\TheLibrary\\ExportHdr"', 
+			'g++ -c "__TRUNK__\\Examples\\Compile\\CPP\\StoreBuildResults\\TheLibrary\\Src\\NameHolder.cpp" -o "__TMP__\\p0\\Outer\\TheLibrary\\Src\\NameHolder.o"  -I"__TRUNK__\\Examples\\Compile\\CPP\\StoreBuildResults\\TheLibrary\\ExportHdr"', 
 			'ar q "__TMP__\\p0\\Outer\\TheLibrary.a" "__TMP__\\p0\\Outer\\TheLibrary\\Src\\NameHolder.o"', 
 			'g++ "__TMP__\\p0\\Obj\\Src\\main.o" "__TMP__\\p0\\Outer\\TheLibrary.a" -o "__TMP__\\p0\\Bin\\UseExportHdr"'
 			], 
@@ -67,3 +68,39 @@ class TestStoringCompilation(unittest.TestCase):
 			)
 		
 		usr1prj = TestStoringCompilation.usr1prj.getProject()
+		def setupTmpdirCallback1(compilation):
+			wsetupDefaultPathsFromRoot(compilation, tmp1)
+		scwe = StoreCallsWillExecutor()
+		wdefaultBuild(usr1prj, setupTmpdirCallback1, scwe)
+		history = "@".join(scwe.history).replace(trunk, "__TRUNK__").replace(tmpDir, "__TMP__").split("@")
+		self.assertEqual(
+			[
+			'g++ -c "__TRUNK__\\Examples\\Compile\\CPP\\StoreBuildResults\\User1\\Src\\main.cpp" -o "__TMP__\\p1\\Obj\\Src\\main.o"  -I"__TRUNK__\\Examples\\Compile\\CPP\\StoreBuildResults\\TheLibrary\\ExportHdr"', 
+			'g++ -c "__TRUNK__\\Examples\\Compile\\CPP\\StoreBuildResults\\TheLibrary\\Src\\NameHolder.cpp" -o "__TMP__\\p1\\Outer\\TheLibrary\\Src\\NameHolder.o"  -I"__TRUNK__\\Examples\\Compile\\CPP\\StoreBuildResults\\TheLibrary\\ExportHdr"', 
+			'ar q "__TMP__\\p1\\Outer\\TheLibrary.a" "__TMP__\\p1\\Outer\\TheLibrary\\Src\\NameHolder.o"', 
+			'g++ "__TMP__\\p1\\Obj\\Src\\main.o" "__TMP__\\p1\\Outer\\TheLibrary.a" -o "__TMP__\\p1\\Bin\\UseExportHdr"'
+			], 
+			history
+			)
+		del history
+		swe = SystemWillExecutor()
+		for cmd in scwe.history:
+			swe.execute(cmd)
+		del scwe
+
+		project = Project(tmp1)
+		project.addSrcFolders("\n".join(os.listdir(project.getRoot())))
+		project.setSourceFilter(ExtensionBasedFilter({"o": "o", "exe": "exe", "a": "a"}));
+		project.findSources()
+		objItems = project.getSourceItems()
+		objPaths = [io.getPathNameExt(1) for io in objItems]
+		objPaths.sort()
+		self.assertEquals(
+			[
+			'Bin\\UseExportHdr.exe', 
+			'Obj\\Src\\main.o', 
+			'Outer\\TheLibrary.a', 
+			'Outer\\TheLibrary\\Src\\NameHolder.o'
+			], 
+			objPaths
+			)
