1adf8d321ebbfcd8f5e388348cd128e3e88d932a
rchbiu <rchbiu@localhost>
2009-06-25 15:10:46 +0400

    .

--- Examples/Compile/CPP/UseExportHeaders/CM/build_useexphdr.py
+++ Examples/Compile/CPP/UseExportHeaders/CM/build_useexphdr.py
@@ -31,6 +31,7 @@ if (__name__ == "__main__"):
 	import os
 	def setupBuildRoot(compilation, project):
 		pass
+		wsetupDefaultPathsFromRoot(compilation, project)
 #		wsetupDefaultPathsFromRoot(compilation, project, "z:\\uaa")
 	project = getProject()
 	wdefaultBuild(project, setupBuildRoot)
