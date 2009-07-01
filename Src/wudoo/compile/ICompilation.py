78b990cc616371b004e68560551243c4dc09bf57
rchbiu <rchbiu@localhost>
2009-07-01 17:39:00 +0400

    debug flag

--- Src/wudoo/compile/ICompilation.py
+++ Src/wudoo/compile/ICompilation.py
@@ -5,17 +5,22 @@ Stories compilation results that was built.
 Holds compiler.
 Stories compilation flags.
 """
-#	def buildWorkFlow(self, willExecutor):
-#		raise NotImplementedError()
-
-#	def getProject(self):
-#		raise NotImplementedError()
-	
 	def buildCompilationResult(self, emptyCompilationResult, willExecutor):
-		"""\
-emptyCompilationResult - comp result with set destination ... 
-but not set obj-s, lib-s, dll-s ...
-"""
+		raise NotImplementedError()
+		
+	def registerBuilder(self, compilationResultClazz, builder):
+		raise NotImplementedError()
+
+	def setCompiler(self, compiler):
+		raise NotImplementedError()
+		
+	def setResolveDependenceStrategy(self, resolveDependenceStrategy):
+		raise NotImplementedError()
+		
+	def getResolveDependenceStrategy(self):
+		raise NotImplementedError()
+
+	def getCompiler(self):
 		raise NotImplementedError()
 	
 	def getAllocateStrategy(self):
@@ -23,37 +28,9 @@ but not set obj-s, lib-s, dll-s ...
 	
 	def setAllocateStrategy(self, allocateStrategy):
 		raise NotImplementedError()
-	   	
-#	def resolveDependings(self, willExecutor):
-#		raise NotImplementedError()
 
-#	def buildBinary(self, willExecutor):
-#		raise NotImplementedError()
-
-#	def getAllObjectItems(self, **params):
-#		raise NotImplementedError()
-
-#	def getSrc2ObjMap(self):
-#		raise NotImplementedError()
-	
-	def getCompiler(self):
+	def setDebugInfoLevel(self, debugInfoLevel):
+		"""\
+debugInfoLevel in [0 .. 100]
+"""		
 		raise NotImplementedError()
-
-	def setCompiler(self, compiler):
-		raise NotImplementedError()
-
-	def setResolveDependenceStrategy(self, resolveDependenceStrategy):
-		raise NotImplementedError()
-
-	def getResolveDependenceStrategy(self):
-		raise NotImplementedError()
-	
-#	def setObjRoot(self, objRoot):
-#		raise NotImplementedError()
-
-#	def setAllocateObjStrategy(self, strat):
-#		raise NotImplementedError()
-
-#	def setDependenceBuildRoot(self, dependenceBuildRoot):
-#		raise NotImplementedError()
-	
