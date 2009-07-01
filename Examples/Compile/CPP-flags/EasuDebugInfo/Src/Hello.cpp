bad1d7424f4a49764f297af8e6ca78d157dbe3d3
rchbiu <rchbiu@localhost>
2009-07-01 21:23:02 +0400

    .

--- Examples/Compile/CPP-flags/EasuDebugInfo/Src/Hello.cpp
+++ Examples/Compile/CPP-flags/EasuDebugInfo/Src/Hello.cpp
@@ -3,9 +3,9 @@
 char * HelloDebug()
 {
 	int sum = 0;
-	for ( int i = 0; i < 10; i++ )
+	for ( int i = 0; i < 100; i++ )
 	{
-		for ( int j = 0; j < 10; j++ )
+		for ( int j = 0; j < 100; j++ )
 		{
 			sum += i * j;
 			if ( sum % 123 == 32 )
