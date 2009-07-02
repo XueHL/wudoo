2c2b8f4a329364aad73e681909ab3afda8288ae5
rchbiu <rchbiu@localhost>
2009-07-02 17:58:47 +0400

    incompl

--- Tests/run_tests.py
+++ Tests/run_tests.py
@@ -14,14 +14,16 @@ import tests.compile.TestCompiler
 import tests.compile.examples.TestEasyExamples
 import tests.compile.examples.TestStoringCompilation
 import tests.console.TestConsole2Obj
+import tests.compile.TestFront
 
 testsuit = unittest.TestSuite([\
-	unittest.TestLoader().loadTestsFromModule(tests.compile.TestProject),\
-	unittest.TestLoader().loadTestsFromModule(tests.compile.TestItemsWork),\
-	unittest.TestLoader().loadTestsFromModule(tests.compile.TestCompilation),\
-	unittest.TestLoader().loadTestsFromModule(tests.compile.TestCompiler),\
-	unittest.TestLoader().loadTestsFromModule(tests.compile.examples.TestEasyExamples),\
-	unittest.TestLoader().loadTestsFromModule(tests.compile.examples.TestStoringCompilation),\
-	unittest.TestLoader().loadTestsFromModule(tests.console.TestConsole2Obj),\
+	#unittest.TestLoader().loadTestsFromModule(tests.compile.TestProject),\
+	#unittest.TestLoader().loadTestsFromModule(tests.compile.TestItemsWork),\
+	#unittest.TestLoader().loadTestsFromModule(tests.compile.TestCompilation),\
+	#unittest.TestLoader().loadTestsFromModule(tests.compile.TestCompiler),\
+	#unittest.TestLoader().loadTestsFromModule(tests.compile.examples.TestEasyExamples),\
+	#unittest.TestLoader().loadTestsFromModule(tests.compile.examples.TestStoringCompilation),\
+	#unittest.TestLoader().loadTestsFromModule(tests.console.TestConsole2Obj),\
+	unittest.TestLoader().loadTestsFromModule(tests.compile.TestFront),\
 	])
 unittest.TextTestRunner(verbosity=2).run(testsuit)
