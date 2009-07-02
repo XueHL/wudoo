d5c1396bc5049661ac3fb2437a708b682ccddce5
rchbiu <rchbiu@localhost>
2009-07-02 17:36:42 +0400

    .

---
+++ Src/wudoo/console/Console2obj.py
@@ -0,0 +1,16 @@
+class Empty:
+	pass
+
+def argArr2obj(argv):
+	obj = Empty()
+	dict = obj.__dict__
+	top = None
+	for it in argv:
+		if it.find("--") > -1:
+			it = it[2:]
+			top = []
+			dict[it] = top
+		else:
+			if top is not None:
+				top.append(it)
+	return obj
