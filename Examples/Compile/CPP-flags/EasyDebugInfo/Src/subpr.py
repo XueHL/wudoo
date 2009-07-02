a6f37aa9620a14aac7a7200a05d5507cd7819bf7
rchbiu <rchbiu@localhost>
2009-07-02 15:09:25 +0400

    .

--- Examples/Compile/CPP-flags/EasyDebugInfo/Src/subpr.py
+++ Examples/Compile/CPP-flags/EasyDebugInfo/Src/subpr.py
@@ -1,4 +1,4 @@
-import subprocess, os
+import subprocess, os, sys
 
 cmd = "g++ " + \
 	" -c " + \
@@ -9,23 +9,42 @@ cmd = "g++ " + \
 ret = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 print "###\n\n", str(ret).replace("\n", "\n\t\t"), "\n\n###\n"
 
-cmd = "ar " + \
-	" q " + \
-	" Hello.a " + \
-	" Hello.o " + \
-	""
-ret = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-print "###\n\n", str(ret).replace("\n", "\n\t\t"), "\n\n###\n"
+def hellolib():
+	shared = len(sys.argv) > 0 and "shared" in sys.argv
+	if shared:
+		cmd = "g++ " + \
+			" -shared " + \
+			" Hello.o " + \
+			" -o " + \
+			" Hello.dll " + \
+			""
+		ret = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+		print "###\n\n", str(ret).replace("\n", "\n\t\t"), "\n\n###\n"
+		return "Hello.dll"
+	else:
+		cmd = "ar " + \
+			" q " + \
+			" Hello.a " + \
+			" Hello.o " + \
+			""
+		ret = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+		print "###\n\n", str(ret).replace("\n", "\n\t\t"), "\n\n###\n"
+		return "Hello.a"
+
+libn = hellolib()
 
 cmd = "g++ " + \
 	" Main.o " + \
-	" Hello.a " + \
+	libn + \
 	" -o " + \
 	" Hello-debug " + \
 	""
 ret = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 print "###\n\n", str(ret).replace("\n", "\n\t\t"), "\n\n###\n"
 
-os.remove("Hello.o")
-os.remove("Hello.a")
-os.remove("Main.o")
+def rm(n):
+	if os.path.exists(n):
+		os.remove(n)
+rm("Hello.o")
+rm("Hello.a")
+rm("Main.o")
