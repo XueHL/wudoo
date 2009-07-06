f8c6c5020cb3ef2fd63535690f47a3fe72cba0db
rchbiu <rchbiu@localhost>
2009-07-06 16:46:35 +0400

    .

---
+++ Examples/Compile/CPP-skip/EasySkip/Src/foo-0-nch.cpp
@@ -0,0 +1,6 @@
+#include "foo-0-nch.h"
+
+char * Foo0()
+{
+	return "Foo-0";
+}
---
+++ Examples/Compile/CPP-skip/EasySkip/Src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "foo-0-nch.h"
+#include "foo-1-ch.h"
+
+int main( int count, char * * args )
+{
+	std::cout << Foo0() << "\n" << Foo1();
+}
