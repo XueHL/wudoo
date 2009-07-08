23cb5a470f948c771949ef31e09b740e9aa31b58
rchbiu <rchbiu@localhost>
2009-07-08 15:01:16 +0400

    .

--- Src/wudoo/compile/buildresult/skipcompile/CompileAllStrategy.py
+++ Src/wudoo/compile/buildresult/skipcompile/CompileAllStrategy.py
@@ -1,8 +1,8 @@
 from wudoo.compile.buildresult.skipcompile.ISkipItemsStrategy import ISkipItemsStrategy
 
 class CompileAllStrategy(ISkipItemsStrategy):
-	def skip(self, srcFsItem, objFsItem):
+	def skip(self, srcFsItem, compilation, project):
 		return False
 
-	def onCompiled(self, srcFsItem, objFsItem):
+	def onCompiled(self, srcFsItem, compilation, project):
 		pass
