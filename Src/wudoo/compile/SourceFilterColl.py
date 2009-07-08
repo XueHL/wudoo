fe8c69c34394018e7ca4c3ded2ac4b86b4f5aaae
rchbiu <rchbiu@localhost>
2009-07-08 18:12:59 +0400

    .

--- Src/wudoo/compile/SourceFilterColl.py
+++ Src/wudoo/compile/SourceFilterColl.py
@@ -2,4 +2,5 @@ from wudoo.filter.AcceptAllFilter import AcceptAllFilter
 from wudoo.filter.ExtensionBasedFilter import ExtensionBasedFilter
 
 CPP_SOURCE_FILTER = ExtensionBasedFilter({"cpp": "cpp"})
+HDR_SOURCE_FILTER = ExtensionBasedFilter({"h": "h"})
 ACCEPT_ALL_FILTER = AcceptAllFilter()
