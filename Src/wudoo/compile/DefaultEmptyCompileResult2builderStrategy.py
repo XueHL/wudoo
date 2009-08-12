6ad3a924477cb58fea278b3b93f8c09e74fda08a
rchbiu <rchbiu@localhost>
2009-08-12 13:48:59 +0400

    DefaultEmptyCompileResult2builderStrategy :: getProjectBasedBuilder.

--- Src/wudoo/compile/DefaultEmptyCompileResult2builderStrategy.py
+++ Src/wudoo/compile/DefaultEmptyCompileResult2builderStrategy.py
@@ -8,4 +8,14 @@ class DefaultEmptyCompileResult2builderStrategy(ICompileResult2builderStrategy):
 		self.__buildersMap[compilationResultClazz] = builder
 	
 	def emptyCompileResult2builder(self, emptyCompilationResult):
+		project = emptyCompilationResult.getProject()
+		prjBasedBuilder = self.getProjectBasedBuilder(project)
+		if prjBasedBuilder is not None:
+			return prjBasedBuilder
 		return self.__buildersMap[emptyCompilationResult.__class__]
+
+	def getProjectBasedBuilder(self, project):
+		try:
+			return project.getSpecialBuilder()
+		except:
+			return None
